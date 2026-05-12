dad47e3ee808fe2ee50bc412744fe1408d14aba9
p-krishna <krishna21.7.97@outlook.com>
2026-05-12 05:32:03 +0530

    outlier kernel crash solved 2026-06-12 05:31

--- segmentation.py
+++ segmentation.py
@@ -13,21 +13,26 @@ pcd = o3d.io.read_point_cloud("sample_data/light-point-cloud-of-my-living-room.p
 #%% Preprocessing
 # get center of the point cloud
 center = pcd.get_center()
-print("Center of the point cloud:", center)
+print("Original Center of the point cloud:", center)
 # translate the point cloud to the origin
 pcd.translate(-center)
+print("Center after translation:", pcd.get_center())
 
 # %% Voxel downsampling
-voxel_downsampled_pcd = pcd.voxel_down_sample(voxel_size=0.1)
-print("Number of points after voxel downsampling:", len(voxel_downsampled_pcd.points))
-o3d.visualization.draw_geometries([voxel_downsampled_pcd], window_name="Voxel Downsampled Point Cloud")
+sample_pcd = pcd.voxel_down_sample(voxel_size=0.01)
+print("Number of points after voxel downsampling:", len(sample_pcd.points))
+o3d.visualization.draw_geometries([sample_pcd], window_name="Voxel Downsampled Point Cloud")
 
 #%% Statistical outlier removal
 near_neighbors = 4
 std_ratio = 1.0
 
-pcd, ind = voxel_downsampled_pcd.remove_statistical_outlier(nb_neighbors=near_neighbors, std_ratio=std_ratio)
+cl, ind = sample_pcd.remove_statistical_outlier(nb_neighbors=near_neighbors, std_ratio=std_ratio)
 
-outlier_cloud = voxel_downsampled_pcd.select_by_index(ind, invert=True)
-outlier_cloud.paint_uniform_color([0, 0, 1])  # Blue color
-o3d.visualization.draw_geometries([outlier_cloud], window_name="Statistical Outlier Removal")
+inlier_cloud = sample_pcd.select_by_index(ind)
+outlier_cloud = sample_pcd.select_by_index(ind, invert=True)
+
+inlier_cloud.paint_uniform_color([1, 0, 0])
+outlier_cloud.paint_uniform_color([0, 0, 1])
+
+o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
